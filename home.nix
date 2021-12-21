fccaba627d6835c9e34e042920ece8ad200ba59d
Ross A. Baker <ross@rossabaker.com>
2021-12-20 22:54:13 -0500

    Configure git user

--- home.nix
+++ home.nix
@@ -18,7 +18,11 @@
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
+    userName = "Ross A. Baker";
+    userEmail = "ross@rossabaker.com";
+  };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
