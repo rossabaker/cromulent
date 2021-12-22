446097eaa204813f438b2aea5473da15d84baaff
Ross A. Baker <ross@rossabaker.com>
2021-12-21 22:16:51 -0500

    Headings

--- modules/emacs/init.el
+++ modules/emacs/init.el
@@ -1,6 +1,8 @@
 (eval-when-compile
   (require 'use-package))
 
+;;; UI
+
 (use-package emacs
   :custom-face
   (default ((t :height 140))))
@@ -12,6 +14,8 @@
 (use-package tool-bar
   :config
   (tool-bar-mode -1))
+
+;;; Tools
 
 (use-package magit
   :ensure)
