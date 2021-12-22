e279dcf95768f59c93f8821de619bea3b56d4d05
Ross A. Baker <ross@rossabaker.com>
2021-12-21 22:27:06 -0500

    startup is more of a UI thing

--- modules/emacs/init.el
+++ modules/emacs/init.el
@@ -1,15 +1,6 @@
 (eval-when-compile
   (require 'use-package))
 
-;;; Core
-
-(use-package startup
-  :no-require
-  :custom
-  (initial-scratch-message nil)
-  (inhibit-startup-echo-area-message t)
-  (inhibit-startup-screen t))
-
 ;;; UI
 
 (use-package emacs
@@ -23,6 +14,13 @@
 (use-package scroll-bar
   :config
   (scroll-bar-mode -1))
+
+(use-package startup
+  :no-require
+  :custom
+  (initial-scratch-message nil)
+  (inhibit-startup-echo-area-message t)
+  (inhibit-startup-screen t))
 
 (use-package tool-bar
   :config
