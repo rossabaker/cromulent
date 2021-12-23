5d601c60dd9d767610ee8ccc590dd0b4019949bb
Ross A. Baker <ross@rossabaker.com>
2021-12-23 14:33:21 -0500

    Enable flakes systemwide

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -13,7 +13,12 @@
 
   # Auto upgrade nix package and the daemon service.
   # services.nix-daemon.enable = true;
-  nix.package = pkgs.nixFlakes;
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
 
   # Create /etc/bashrc that loads the nix-darwin environment.
   programs.zsh.enable = true;  # default shell on catalina
