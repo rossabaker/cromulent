7d03a8cc26e194f588ed6b8ce826e893fa2b4470
Ross A. Baker <ross@rossabaker.com>
2022-01-04 17:26:32 -0500

    Add an smerge hydra

--- modules/emacs/init.el
+++ modules/emacs/init.el
@@ -131,6 +131,9 @@ is already installed.  This is true in our Nix environment."
   :hook
   (after-init-hook . gcmh-mode))
 
+(use-package hydra
+  :ensure)
+
 (use-package minibuf-eldef
   :config
   (minibuffer-electric-default-mode))
@@ -365,6 +368,45 @@ is already installed.  This is true in our Nix environment."
 
 (use-package restclient
   :ensure)
+
+(use-package smerge-mode
+  :config
+  (defhydra ross/hydra-smerge
+    ;; Credit: https://github.com/kaushalmodi/.emacs.d
+    (:color pink
+     :hint nil
+     :pre (smerge-mode 1)
+     ;; Disable `smerge-mode' when quitting hydra if no merge
+     ;; conflicts remain.
+     :post (smerge-auto-leave))
+    "
+^Move^       ^Keep^               ^Diff^                 ^Other^
+^^-----------^^-------------------^^---------------------^^-------
+_n_ext       _b_ase               _<_: upper/base        _C_ombine
+_p_rev       _u_pper              _=_: upper/lower       _r_esolve
+^^           _l_ower              _>_: base/lower        _k_ill current
+^^           _a_ll                _R_efine
+^^           _RET_: current       _E_diff
+"
+    ("n" smerge-next)
+    ("p" smerge-prev)
+    ("b" smerge-keep-base)
+    ("u" smerge-keep-upper)
+    ("l" smerge-keep-lower)
+    ("a" smerge-keep-all)
+    ("RET" smerge-keep-current)
+    ("\C-m" smerge-keep-current)
+    ("<" smerge-diff-base-upper)
+    ("=" smerge-diff-upper-lower)
+    (">" smerge-diff-base-lower)
+    ("R" smerge-refine)
+    ("E" smerge-ediff)
+    ("C" smerge-combine-with-next)
+    ("r" smerge-resolve)
+    ("k" smerge-kill-current)
+    ("q" nil "cancel" :color blue))
+  :bind
+  ("C-c g m" . ross/hydra-smerge/body))
 
 ;;; Languages
 
