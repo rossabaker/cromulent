ecd723396dda91ac4e987b8ef7734360d3c98e36
Ross A. Baker <ross@rossabaker.com>
2022-01-14 08:12:25 -0500

    Add sbt-updates plugin globally

--- modules/scala/default.nix
+++ modules/scala/default.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }:
 
 {
-  home.file.".sbt/1.0/sonatype.sbt".source = ./sonatype.sbt;
+  home.file = {
+    ".sbt/1.0/sonatype.sbt".source = ./sonatype.sbt;
+    ".sbt/1.0/plugins/sbt-updates.sbt".source = ./sbt-updates.sbt;
+  };
 
   home.packages = [
     pkgs.sbt
