1dc5b146b04162419672db08dc44a869a2f9474d
Ross A. Baker <ross@rossabaker.com>
2022-01-19 23:45:28 -0500

    Add Typelevel cachix

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -18,10 +18,12 @@
     binaryCaches = [
       "https://nix-community.cachix.org/"
       "https://rossabaker.cachix.org/"
+      "https://typelevel.cachix.org/"
     ];
     binaryCachePublicKeys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "rossabaker.cachix.org-1:KK/CQTeAGEurCUBy3nDl9PdR+xX+xtWQ0C/GpNN6kuw="
+      "typelevel.cachix.org-1:UnD9fMAIpeWfeil1V/xWUZa2g758ZHk8DvGCd/keAkg="
     ];
     package = pkgs.nixFlakes;
     extraOptions = ''
