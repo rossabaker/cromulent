98f788405780caf5ab65d049a61899e47931ad3b
Ross A. Baker <ross@rossabaker.com>
2022-04-10 00:42:49 -0400

    Add header nav

--- pkgs/website/default.nix
+++ pkgs/website/default.nix
@@ -1,17 +1,22 @@
 { emacs, stdenv }:
 
-stdenv.mkDerivation {
-  name = "rossabaker.com";
-  src = ./src;
-  buildInputs = [ emacs ];
-  buildPhase = ''
-    echo $TMPDIR
-    export HOME=$TMPDIR
-    ${emacs}/bin/emacs -Q --script publish.el $HOME/html
-    echo 'rossabaker.com' > $HOME/html/CNAME
-  '';
-  installPhase = ''
-    mkdir $out
-    cp -a $HOME/html/* $out
-  '';
-}
+let
+  siteEmacs = emacs.pkgs.withPackages (epkgs: [
+    epkgs.esxml
+  ]);
+in
+  stdenv.mkDerivation {
+    name = "rossabaker.com";
+    src = ./src;
+    buildInputs = [ siteEmacs ];
+    buildPhase = ''
+      echo $TMPDIR
+      export HOME=$TMPDIR
+      ${siteEmacs}/bin/emacs -Q --script publish.el $HOME/html
+      echo 'rossabaker.com' > $HOME/html/CNAME
+    '';
+    installPhase = ''
+      mkdir $out
+      cp -a $HOME/html/* $out
+    '';
+  }
--- pkgs/website/src/publish.el
+++ pkgs/website/src/publish.el
@@ -1,8 +1,16 @@
 (require 'ox-publish)
 (require 'ox-html)
+(require 'esxml)
 
 (defun rossabaker.com/out ()
   (or (car command-line-args-left) (expand-file-name "../html")))
+
+(defun rossabaker.com/header ()
+  (sxml-to-xml
+   '(nav
+     (ul
+      (li (a (@ (href "/")) "Home"))
+      (li (a (@ (href "/talks")) "Talks"))))))
 
 (setq org-export-with-section-numbers nil
       org-export-with-toc nil
@@ -14,7 +22,8 @@
 	 :base-extension "org"
 	 :recursive nil
 	 :publishing-directory ,(rossabaker.com/out)
-	 :publishing-function org-html-publish-to-html)
+	 :publishing-function org-html-publish-to-html
+	 :html-head ,(rossabaker.com/header))
 	("blog"
 	 :base-directory ,(expand-file-name "org/blog/")
 	 :base-extension "org"
@@ -23,7 +32,8 @@
 	 :auto-sitemap t
 	 :sitemap-title "Blog Posts"
 	 :sitemap-filename "index.org"
-	 :sitemap-sort-files anti-chronologically)
+	 :sitemap-sort-files anti-chronologically
+	 :html-head ,(rossabaker.com/header))
 	("talks"
 	 :base-directory ,(expand-file-name "org/talks/")
 	 :base-extension "org"
@@ -32,7 +42,8 @@
 	 :auto-sitemap t
 	 :sitemap-title "Talks"
 	 :sitemap-filename "index.org"
-	 :sitemap-sort-files anti-chronologically)
+	 :sitemap-sort-files anti-chronologically
+	 :html-head ,(rossabaker.com/header))
 	("rossabaker.com" :components ("blog" "talks" "pages"))))
 
 (org-publish-all t)
