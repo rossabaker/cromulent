65fa2e6678e4f482e8817ea400aebb522ca65f9e
Ross A. Baker <ross@rossabaker.com>
2022-04-28 13:36:33 -0400

    Remove Bitter font

--- src/publish.el
+++ src/publish.el
@@ -15,7 +15,7 @@
   (mapconcat 'esxml-to-xml
    `((link ((rel . "preconnect") (href . "https://fonts.googleapis.com")))
      (link ((rel . "preconnect") (href . "https://fonts.gstatic.com") (crossorigin . "anonymous")))
-     (link ((rel . "stylesheet") (href . "https://fonts.googleapis.com/css2?family=Bitter:wght@800&family=Fira+Code:wght@500&family=Fira+Sans&display=swap")))
+     (link ((rel . "stylesheet") (href . "https://fonts.googleapis.com/css2?family=Fira+Code:wght@500&family=Fira+Sans&display=swap")))
      (link ((href . "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css")
 	    (rel . "stylesheet")
 	    (integrity . "sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3")
