a8bb895ea3c23107aba57c049a5673ec62394e3f
Ross A. Baker <ross@rossabaker.com>
2022-04-29 20:52:23 -0400

    Fix navbar toggle

--- src/publish.el
+++ src/publish.el
@@ -58,7 +58,7 @@
 		       (aria-controls . "navbarSupportedContent")
 		       (aria-expanded . "false")
 		       (aria-label . "Toggle navigation"))
-		      (span ((class . "navbar-toggler-icon"))))
+		      (span ((class . "navbar-toggler-icon")) nil))
 	      (div ((class . "collapse navbar-collapse")
 		    (id . "navbarSupportedContent"))
 		   (ul ((class . "navbar-nav"))
@@ -85,7 +85,8 @@
   (esxml-to-xml
    '(script ((src . "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js")
 	     (integrity . "sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p")
-	     (crossorigin . "anonymous")))))
+	     (crossorigin . "anonymous"))
+	    nil)))
 
 (setq org-export-with-section-numbers nil
       org-export-with-toc nil
