afa89935e38c4661ff614fc1f80fc3789e16c39b
Ross A. Baker <ross@rossabaker.com>
2022-05-05 01:11:20 -0400

    Move emacs config to own directory

--- modules/emacs/default.nix
+++ modules/emacs/default.nix
@@ -8,12 +8,12 @@ let
 
   emacs-config = pkgs.stdenv.mkDerivation {
     name = "emacs-config";
-    src = ../../src/org/config;
+    src = ../../src/org/config/emacs;
     buildInputs = [
       emacs
     ];
     buildPhase = ''
-      ${emacs}/bin/emacs -Q -nw emacs.org --batch -f org-babel-tangle --kill
+      ${emacs}/bin/emacs -Q -nw index.org --batch -f org-babel-tangle --kill
     '';
     installPhase = ''
       mkdir -p $out/share/emacs/site-lisp
