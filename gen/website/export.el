3a0ea61522ae68b727f582bdcb0ecb34d2dbfd18
Ross A. Baker <ross@rossabaker.com>
2022-08-25 14:39:12 -0400

    Generate chroma.css for syntax highlighting

--- gen/website/export.el
+++ gen/website/export.el
@@ -1,5 +1,8 @@
 (require 'ox-hugo)
+(require 'ob-shell)
+
 (find-file "src/org/rossabaker.org")
+(setq org-confirm-babel-evaluate nil)
 (org-babel-tangle)
 (mkdir "../../.hugo-out/static")
 (org-hugo-export-wim-to-md t)
