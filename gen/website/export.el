2d413ff215688468e6c2ed36920da6d8c3a70fdd
Ross A. Baker <ross@rossabaker.com>
2022-08-28 00:13:01 -0400

    Add PGP public key to contact

--- gen/website/export.el
+++ gen/website/export.el
@@ -1,6 +1,7 @@
 (require 'ox-hugo)
 (require 'ob-shell)
 
+(put 'org-hugo-external-file-extensions-allowed-for-copying 'safe-local-variable 'listp)
 (find-file "src/org/rossabaker.org")
 (setq org-confirm-babel-evaluate nil)
 (org-babel-tangle)
