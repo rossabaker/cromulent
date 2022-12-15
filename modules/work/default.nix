0ceaf4c9e6f860649b2343f141e36994413e947c
Ross A. Baker <ross@rossabaker.com>
2022-12-15 00:23:33 -0500

    Add Rectangle on Darwin

--- modules/work/default.nix
+++ modules/work/default.nix
@@ -5,7 +5,10 @@
     inputs.firefox-darwin.overlay
   ];
 
-  home.packages = [ pkgs.firefox-bin ];
+  home.packages = [
+    pkgs.firefox-bin
+    pkgs.rectangle
+  ];
 
   home.file = {
     ".sbt/1.0/work.sbt".source = ./work.sbt;
