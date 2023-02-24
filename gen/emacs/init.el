2015d838c014c0e40db7b362d7dd1cf331460438
Ross A. Baker <ross@rossabaker.com>
2023-02-24 14:45:09 -0500

    A less distracting cursor

--- gen/emacs/init.el
+++ gen/emacs/init.el
@@ -4,6 +4,13 @@
   (initial-major-mode 'fundamental-mode)
   (initial-scratch-message nil))
 
+(use-package emacs
+  :custom
+  (cursor-type 'bar))
+(use-package frame
+  :config
+  (blink-cursor-mode -1))
+
 (use-package scroll-bar
   :config
   (scroll-bar-mode -1))
