2b0f476d2e2af8ddf00ae0b24351fc0955071019
Ross A. Baker <ross@rossabaker.com>
2023-03-03 19:42:32 -0500

    Garbage collector magic hack

--- gen/emacs/init.el
+++ gen/emacs/init.el
@@ -5,6 +5,12 @@
   :config
   (benchmark-init/activate))
 
+(use-package gcmh
+  :ensure t
+  :diminish
+  :init (setq gc-cons-threshold (* 80 1024 1024))
+  :hook (emacs-startup . gcmh-mode))
+
 (use-package on
   :ensure)
 
@@ -92,17 +98,17 @@
   (set-mark-command-repeat-pop t))
 
 (use-package magit
-   :ensure t
-   :defer 1
-   :bind ("C-c g g" . magit-status)
-   :custom
-   (magit-clone-default-directory "~/src/")
-   :config
-   (defun ross/magit-clone-read-args-a (orig-fun &rest args)
-     "Sets vertico-preselect to 'prompt when cloning repos, so we clone to the default prompted directory, and not some random existing directory under magit-clone-default-directory."
-     (let ((vertico-preselect 'prompt))
-       (apply orig-fun args)))
-   (advice-add 'magit-clone-read-args :around #'ross/magit-clone-read-args-a))
+  :ensure t
+  :defer 1
+  :bind ("C-c g g" . magit-status)
+  :custom
+  (magit-clone-default-directory "~/src/")
+  :config
+  (defun ross/magit-clone-read-args-a (orig-fun &rest args)
+    "Sets vertico-preselect to 'prompt when cloning repos, so we clone to the default prompted directory, and not some random existing directory under magit-clone-default-directory."
+    (let ((vertico-preselect 'prompt))
+      (apply orig-fun args)))
+  (advice-add 'magit-clone-read-args :around #'ross/magit-clone-read-args-a))
 
 (use-package git-link
   :ensure t
