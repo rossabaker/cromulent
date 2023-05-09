30c643336ff26c70d1775bb54e1cee508b1846c1
Ross A. Baker <ross@rossabaker.com>
2023-05-09 09:47:04 -0400

    Configure full SBT dependency tree plugin

--- gen/scala/default.nix
+++ gen/scala/default.nix
@@ -5,6 +5,7 @@
     ".sbt/1.0/sonatype.sbt".source = ./sonatype.sbt;
     ".sbt/1.0/plugins/sbt-rewarn.sbt".source = ./sbt-rewarn.sbt;
     ".sbt/1.0/plugins/sbt-updates.sbt".source = ./sbt-updates.sbt;
+    ".sbt/1.0/plugins/dependency-tree.sbt".source = ./dependency-tree.sbt;
   };
 
   home.packages = [
