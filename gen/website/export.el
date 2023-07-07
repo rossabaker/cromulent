49e9b311a8fbc5a1e761dcdc92a27129d37e880d
Ross A. Baker <ross@rossabaker.com>
2023-07-07 19:26:10 -0400

    Rename rossabaker.org to cromulent.org

--- gen/website/export.el
+++ gen/website/export.el
@@ -1,7 +1,7 @@
 (require 'ox-hugo)
 (require 'ob-shell)
 
-(with-current-buffer (find-file-noselect "src/org/rossabaker.org")
+(with-current-buffer (find-file-noselect "src/org/cromulent.org")
   (let ((org-confirm-babel-evaluate nil))
     (org-babel-tangle)
     (org-hugo-export-wim-to-md t)))
