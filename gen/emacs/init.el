2dfc01dd3078fbcf6ebe9327bfae11d54b1c4b6b
Ross A. Baker <ross@rossabaker.com>
2023-08-26 18:50:20 -0400

    Render license in the Nix build

--- gen/emacs/init.el
+++ gen/emacs/init.el
@@ -188,21 +188,6 @@ with EXPORT_FILE_NAME."
 `(setq org-babel-exp-code-template ,org-babel-exp-code-template)
 )
 
-(defun rab/org-hugo-src-block (src-block _contents info)
-  (let* ((result (org-hugo-src-block src-block _contents info))
-	 (block-info
-	  (org-with-point-at (org-element-property :begin src-block)
-	    (org-babel-get-src-block-info)))
-	 (license (assoc-default :code-license (elt block-info 2))))
-    (if (member license '("%code-license" ""))
-	result
-      (format "<div class=\"code-snippet\"><p class=\"license\">%s</p>\n\n%s\n\n</div>" license result))))
-
-(with-eval-after-load 'ox-hugo
-  (map-put!
-   (org-export-backend-transcoders (org-export-get-backend 'hugo))
-   'src-block 'rab/org-hugo-src-block))
-
 (use-package ox-slack
   :ensure t
   :after org
