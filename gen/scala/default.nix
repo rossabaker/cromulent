dc1e4fdea1337b36bb77ce8e83928df1ebfcc160
Ross A. Baker <ross@rossabaker.com>
2023-09-08 13:53:03 -0500

    Add filenames to Scala examples

--- gen/scala/default.nix
+++ gen/scala/default.nix
@@ -6,7 +6,7 @@
         ".sbt/1.0/plugins/sbt-dependency-submission.sbt".source = ./sbt-dependency-submission.sbt;
               ".sbt/1.0/plugins/sbt-rewarn.sbt".source = ./sbt-rewarn.sbt;
               ".sbt/1.0/plugins/sbt-updates.sbt".source = ./sbt-updates.sbt;
-              ".sbt/1.0/plugins/dependency-tree.sbt".source = ./dependency-tree.sbt;
+              ".sbt/1.0/plugins/sbt-dependency-tree.sbt".source = ./sbt-dependency-tree.sbt;
       };
 
       home.packages = [
