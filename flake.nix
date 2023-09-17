f495614e1559a6f0e817b8e1df8e104ff4d80916
Ross A. Baker <ross@rossabaker.com>
2023-09-17 01:05:35 -0400

    Refactor flake away from noweb

--- flake.nix
+++ flake.nix
@@ -1,244 +1,86 @@
 {
   description = "Ross A. Baker's perfectly cromulent Nix flake";
-  inputs =
-    {
-      flake-parts.url = "github:hercules-ci/flake-parts";
-    
-      # Core nix flakes
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    
-      # Home manager flake
-      home-manager.url = "github:nix-community/home-manager";
-      home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    
-      # nix-darwin flake
-      nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-22.11-darwin";
-      darwin.url = "github:LnL7/nix-darwin/master";
-      darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
-    
-      # Extra community flakes
-      devshell.url = "github:numtide/devshell";
-      devshell.inputs.nixpkgs.follows = "nixpkgs";
-    
-      emacs-overlay.url = "github:nix-community/emacs-overlay";
-      emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
-    
-      firefox-darwin.url = "github:bandithedoge/nixpkgs-firefox-darwin";
-      firefox-darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
-    
-      # Emacs packages
-      ammonite-term-repl = {
-        url = "github:zwild/ammonite-term-repl";
-        flake = false;
-      };
-    
-      fill-sentences-correctly = {
-        url = "github:duckwork/fill-sentences-correctly.el";
-        flake = false;
-      };
-    
-      git-related = {
-        url = "git+https://codeberg.org/rossabaker/git-related";
-        flake = false;
-      };
-    
-      hocon-mode = {
-        url = "github:jxq0/hocon-mode";
-        flake = false;
-      };
-    
-      ob-ammonite = {
-        url = "github:zwild/ob-ammonite";
-        flake = false;
-      };
-    
-      on-el = {
-        url = "gitlab:ajgrf/on.el";
-        flake = false;
-      };
-    
-      scala-mode = {
-        url = "github:Kazark/emacs-scala-mode?ref=scala3";
-        flake = false;
-      };
-    
-      unmodified-buffer = {
-        url = "github:arthurcgusmao/unmodified-buffer";
-        flake = false;
-      };
-    
-      emacs-src.url = "github:emacs-mirror/emacs/emacs-29";
-      emacs-src.flake = false;
-    
-      jinx = {
-        url = "github:minad/jinx";
-        flake = false;
-      };
-    
-      # Disabled pending license
-      #   copilot-el = {
-      #     url = "github:zerolfx/copilot.el";
-      #     flake = false;
-      #   };
+
+  outputs = inputs: import ./gen/flake/outputs inputs;
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    # Core nix flakes
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Home manager flake
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # nix-darwin flake
+    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-22.11-darwin";
+    darwin.url = "github:LnL7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
+
+    # Extra community flakes
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+    emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
+
+    firefox-darwin.url = "github:bandithedoge/nixpkgs-firefox-darwin";
+    firefox-darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
+
+    # Emacs packages
+    ammonite-term-repl = {
+      url = "github:zwild/ammonite-term-repl";
+      flake = false;
     };
-  outputs =
-    inputs:
-    
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
-      { withSystem, flake-parts-lib, ... }:
-    
-      let
-        inherit (flake-parts-lib) importApply;
-    
-        flakeModules = {
-          homeModules = ./gen/flake/homeModules;
-          darwinModules = ./gen/flake/darwinModules;
-          garnix = ./gen/flake/garnix;
-          podman = ./gen/flake/podman;
-          postgresql = ./gen/flake/postgresql;
-          nixDarwin = importApply ./gen/nix-darwin { inherit (inputs) self; };
-          emacs = ./gen/emacs;
-          scala = ./gen/scala;
-          python = ./gen/python;
-          modernTs = ./gen/modern_ts;
-          homeManager = ./gen/home-manager;
-          hyperlink = ./gen/flake/hyperlink;
-        };
-    
-        mkHomeConfig = { pkgs, system, username, homeDirectory }:
-          inputs.home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            modules = [
-              {
-                home = {
-                  inherit homeDirectory username;
-                  stateVersion = "21.11";
-                };
-                nixpkgs.overlays = [ inputs.emacs-overlay.overlay ];
-              }
-              inputs.self.homeModules.base
-              inputs.self.homeModules.emacs
-              inputs.self.homeModules.scala
-              inputs.self.homeModules.python
-              ./modules/work
-            ];
-            # Pass our flake inputs into the config
-            extraSpecialArgs = { inherit inputs; };
-          };
-    
-        overlays = {
-          emacs = inputs.emacs-overlay.overlay;
-          devshell = inputs.devshell.overlays.default;
-        };
-    
-        pkgsFor = system: import inputs.nixpkgs {
-          inherit system;
-          overlays = builtins.attrValues inputs.self.overlays;
-        };
-      in {
-        imports = [
-          flakeModules.homeModules
-          flakeModules.darwinModules
-          flakeModules.garnix
-          flakeModules.podman
-          flakeModules.postgresql
-          flakeModules.nixDarwin
-          flakeModules.emacs
-          flakeModules.scala
-          flakeModules.python
-          flakeModules.modernTs
-          flakeModules.homeManager
-          flakeModules.hyperlink
-          inputs.flake-parts.flakeModules.easyOverlay
-        ];
-    
-        flake = {
-          inherit overlays;
-    
-          homeConfigurations = {
-            "RABaker@L2LYQM57XY" = mkHomeConfig {
-              pkgs = (pkgsFor "aarch64-darwin");
-              system = "aarch64-darwin";
-              username = "RABaker";
-              homeDirectory = "/Users/RABaker";
-            };
-          };
-    
-          inherit flakeModules;
-        };
-    
-        systems = [
-          "x86_64-linux"
-          "aarch64-darwin"
-        ];
-    
-        perSystem = { config, self', inputs', system, pkgs, ... }:
-          let
-            hm = inputs.home-manager.defaultPackage."${system}";
-    
-            darwinPackages =
-              if (system == "aarch64-darwin") then {
-                aarch64-darwin-config-base = (inputs.darwin.lib.darwinSystem {
-                  system = "aarch64-darwin";
-                  modules = [ inputs.self.darwinModules.default ];
-                }).system;
-              } else { };
-          in
-            {
-              _module.args.pkgs = import inputs.nixpkgs {
-                inherit system;
-                overlays = [
-                  inputs.devshell.overlays.default
-                  inputs.emacs-overlay.overlays.default
-                  (final: prev: {
-                    hyperlink = config.packages.hyperlink;
-                  })
-                ];
-              };
-    
-              apps = {
-                hello = {
-                  type = "app";
-                  program = "${pkgs.hello}/bin/hello";
-                };
-              };
-    
-              packages = {
-                website = pkgs.callPackage ./gen/website {
-                  emacs = self'.packages.emacs-ross;
-                  src = ./src;
-                };
-              } // darwinPackages;
-    
-              devShells.default = pkgs.devshell.mkShell {
-                name = "cromulent";
-    
-                commands = [
-                  {
-                    name = "hm-switch";
-                    help = "switch the home-manager config";
-                    command = "${hm}/bin/home-manager switch --flake $PRJ_ROOT";
-                  }
-                  {
-                    name = "serve";
-                    help = "run 'hugo serve' on the local project";
-                    command = "(cd $PRJ_ROOT && ${pkgs.hugo}/bin/hugo serve --disableFastRender --config tmp/hugo/config.toml)";
-                  }
-                ];
-    
-                packages = [
-                  hm
-                  pkgs.google-cloud-sdk
-                  pkgs.hugo
-                  pkgs.nix
-                  pkgs.terraform
-                ];
-              };
-    
-              overlayAttrs = {
-                hyperlink = config.packages.hyperlink;
-              };
-            };
-      }
-    );
+
+    fill-sentences-correctly = {
+      url = "github:duckwork/fill-sentences-correctly.el";
+      flake = false;
+    };
+
+    git-related = {
+      url = "git+https://codeberg.org/rossabaker/git-related";
+      flake = false;
+    };
+
+    hocon-mode = {
+      url = "github:jxq0/hocon-mode";
+      flake = false;
+    };
+
+    ob-ammonite = {
+      url = "github:zwild/ob-ammonite";
+      flake = false;
+    };
+
+    on-el = {
+      url = "gitlab:ajgrf/on.el";
+      flake = false;
+    };
+
+    scala-mode = {
+      url = "github:Kazark/emacs-scala-mode?ref=scala3";
+      flake = false;
+    };
+
+    unmodified-buffer = {
+      url = "github:arthurcgusmao/unmodified-buffer";
+      flake = false;
+    };
+
+    emacs-src.url = "github:emacs-mirror/emacs/emacs-29";
+    emacs-src.flake = false;
+
+    jinx = {
+      url = "github:minad/jinx";
+      flake = false;
+    };
+
+    # Disabled pending license
+    #   copilot-el = {
+    #     url = "github:zerolfx/copilot.el";
+    #     flake = false;
+    #   };
+  };
 }
---
+++ gen/flake/outputs/default.nix
@@ -0,0 +1,159 @@
+inputs:
+
+inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+  { withSystem, flake-parts-lib, ... }:
+
+  let
+    inherit (flake-parts-lib) importApply;
+
+    flakeModules = {
+      homeModules = ../homeModules;
+      darwinModules = ../darwinModules;
+      garnix = ../garnix;
+      podman = ../podman;
+      postgresql = ../postgresql;
+      nixDarwin = importApply ../../nix-darwin { inherit (inputs) self; };
+      emacs = ../../emacs;
+      scala = ../../scala;
+      python = ../../python;
+      modernTs = ../../modern_ts;
+      homeManager = ../../home-manager;
+      hyperlink = ../hyperlink;
+    };
+
+    mkHomeConfig = { pkgs, system, username, homeDirectory }:
+      inputs.home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          {
+            home = {
+              inherit homeDirectory username;
+              stateVersion = "21.11";
+            };
+            nixpkgs.overlays = [ inputs.emacs-overlay.overlay ];
+          }
+          inputs.self.homeModules.base
+          inputs.self.homeModules.emacs
+          inputs.self.homeModules.scala
+          inputs.self.homeModules.python
+          ./modules/work
+        ];
+        # Pass our flake inputs into the config
+        extraSpecialArgs = { inherit inputs; };
+      };
+
+    overlays = {
+      emacs = inputs.emacs-overlay.overlay;
+      devshell = inputs.devshell.overlays.default;
+    };
+
+    pkgsFor = system: import inputs.nixpkgs {
+      inherit system;
+      overlays = builtins.attrValues inputs.self.overlays;
+    };
+  in {
+    imports = [
+      flakeModules.homeModules
+      flakeModules.darwinModules
+      flakeModules.garnix
+      flakeModules.podman
+      flakeModules.postgresql
+      flakeModules.nixDarwin
+      flakeModules.emacs
+      flakeModules.scala
+      flakeModules.python
+      flakeModules.modernTs
+      flakeModules.homeManager
+      flakeModules.hyperlink
+      inputs.flake-parts.flakeModules.easyOverlay
+    ];
+
+    flake = {
+      inherit overlays;
+
+      homeConfigurations = {
+        "RABaker@L2LYQM57XY" = mkHomeConfig {
+          pkgs = (pkgsFor "aarch64-darwin");
+          system = "aarch64-darwin";
+          username = "RABaker";
+          homeDirectory = "/Users/RABaker";
+        };
+      };
+
+      inherit flakeModules;
+    };
+
+    systems = [
+      "x86_64-linux"
+      "aarch64-darwin"
+    ];
+
+    perSystem = { config, self', inputs', system, pkgs, ... }:
+      let
+        hm = inputs.home-manager.defaultPackage."${system}";
+
+        darwinPackages =
+          if (system == "aarch64-darwin") then {
+            aarch64-darwin-config-base = (inputs.darwin.lib.darwinSystem {
+              system = "aarch64-darwin";
+              modules = [ inputs.self.darwinModules.default ];
+            }).system;
+          } else { };
+      in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.devshell.overlays.default
+              inputs.emacs-overlay.overlays.default
+              (final: prev: {
+                hyperlink = config.packages.hyperlink;
+              })
+            ];
+          };
+
+          apps = {
+            hello = {
+              type = "app";
+              program = "${pkgs.hello}/bin/hello";
+            };
+          };
+
+          packages = {
+            website = pkgs.callPackage ../../website {
+              emacs = self'.packages.emacs-ross;
+              src = ./src;
+            };
+          } // darwinPackages;
+
+          devShells.default = pkgs.devshell.mkShell {
+            name = "cromulent";
+
+            commands = [
+              {
+                name = "hm-switch";
+                help = "switch the home-manager config";
+                command = "${hm}/bin/home-manager switch --flake $PRJ_ROOT";
+              }
+              {
+                name = "serve";
+                help = "run 'hugo serve' on the local project";
+                command = "(cd $PRJ_ROOT && ${pkgs.hugo}/bin/hugo serve --disableFastRender --config tmp/hugo/config.toml)";
+              }
+            ];
+
+            packages = [
+              hm
+              pkgs.google-cloud-sdk
+              pkgs.hugo
+              pkgs.nix
+              pkgs.terraform
+            ];
+          };
+
+          overlayAttrs = {
+            hyperlink = config.packages.hyperlink;
+          };
+        };
+  }
+)
