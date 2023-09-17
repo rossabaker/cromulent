f702d99cb6a53645e0e1f60263eb19ed583b9755
Ross A. Baker <ross@rossabaker.com>
2023-09-16 23:33:33 -0400

    Fix broken Postgres module

--- gen/flake/postgresql/default.nix
+++ gen/flake/postgresql/default.nix
@@ -9,47 +9,48 @@ in
 {
   flake.darwinModules.postgresql = (
     { config, pkgs, ... }:
-
-    services = {
-      postgresql = {
-        inherit dataDir port;
-        enable = true;
-        package = pkgs.postgresql_14;
-        initdbArgs = [ "-D" dataDir ];
-      };
-    };
-
-    users = {
-      knownUsers = [ "postgres" ];
-      users = {
-        postgres = {
-          uid = 2000;
-          shell = "/bin/bash";
+    {
+      services = {
+        postgresql = {
+          inherit dataDir port;
+          enable = true;
+          package = pkgs.postgresql_14;
+          initdbArgs = [ "-D" dataDir ];
         };
       };
-    };
 
-    # Create the PostgreSQL data directory, if it does not exist.
-    system.activationScripts.preActivation = {
-      enable = true;
-      text = ''
-      if [ ! -d "${dataDir}" ]; then
-        echo "creating PostgreSQL data directory..."
-        sudo mkdir -m 700 -p "${dataDir}/"
-        chown -R ${user}:staff "${dataDir}/"
-      fi
+      users = {
+        knownUsers = [ "postgres" ];
+        users = {
+          postgres = {
+            uid = 2000;
+            shell = "/bin/bash";
+          };
+        };
+      };
 
-      if [ ! -d "/var/log/postgresql" ]; then
-        echo "creating PostgreSQL log directory..."
-        sudo mkdir -m 700 -p "${logDir}/"
-        chown -R ${user}:staff "${logDir}/"
-      fi
-    '';
-    };
+      # Create the PostgreSQL data directory, if it does not exist.
+      system.activationScripts.preActivation = {
+        enable = true;
+        text = ''
+        if [ ! -d "${dataDir}" ]; then
+          echo "creating PostgreSQL data directory..."
+          sudo mkdir -m 700 -p "${dataDir}/"
+          chown -R ${user}:staff "${dataDir}/"
+        fi
 
-    launchd.user.agents.postgresql.serviceConfig = {
-      StandardErrorPath = "${logDir}/postgres.error.log";
-      StandardOutPath = "${logDir}/postgres.out.log";
-    };
+        if [ ! -d "/var/log/postgresql" ]; then
+          echo "creating PostgreSQL log directory..."
+          sudo mkdir -m 700 -p "${logDir}/"
+          chown -R ${user}:staff "${logDir}/"
+        fi
+      '';
+      };
+
+      launchd.user.agents.postgresql.serviceConfig = {
+        StandardErrorPath = "${logDir}/postgres.error.log";
+        StandardOutPath = "${logDir}/postgres.out.log";
+      };
+    }
   );
 }
