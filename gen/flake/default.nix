6da4d962227bcd659d83ad5654cfdf4a5c856b26
Ross A. Baker <ross@rossabaker.com>
2023-09-24 22:44:59 -0400

    Fix the example home-manager config

--- gen/flake/default.nix
+++ gen/flake/default.nix
@@ -51,90 +51,91 @@ flake-parts.lib.mkFlake { inherit inputs; } (
         nixpkgs.overlays = [ emacs-overlay.overlay ];
       };
 
-      homeConfigurations.aarch64-darwin-example = {
-        extraSpecialArgs = { inherit inputs; };
-        pkgs = import nixpkgs {
-          system = "aarch64-darwin";
+      homeConfigurations.aarch64-darwin-example =
+        home-manager.lib.homeManagerConfiguration {
+          extraSpecialArgs = { inherit inputs; };
+          pkgs = import nixpkgs {
+            system = "aarch64-darwin";
+          };
+          modules = [
+            self.homeModules.default
+            {
+              home = rec {
+                username = "Example";
+                homeDirectory = "/Users/${username}";
+              };
+            }
+          ];
         };
-        modules = [
-          self.homeModules.default
-          {
-            home = rec {
-              username = "Example";
-              homeDirectory = "/Users/${username}";
-            };
-          }
-        ];
-      };
     };
 
-    systems = [
-      "x86_64-linux"
-      "aarch64-darwin"
-    ];
+  systems = [
+    "x86_64-linux"
+    "aarch64-darwin"
+  ];
 
-    perSystem = { config, self', inputs', system, pkgs, ... }:
-      let
-        hm = home-manager.defaultPackage."${system}";
+  perSystem = { config, self', inputs', system, pkgs, ... }:
+    let
+      hm = home-manager.defaultPackage."${system}";
 
-        darwinPackages =
-          if (system == "aarch64-darwin") then {
-            aarch64-darwin-config-base = (nix-darwin.lib.darwinSystem {
-              system = "aarch64-darwin";
-              modules = [ self.darwinModules.default ];
-            }).system;
-          } else { };
-      in
-        {
-          _module.args.pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              devshell.overlays.default
-              emacs-overlay.overlays.default
-            ];
-          };
+      darwinPackages =
+        if (system == "aarch64-darwin") then {
+          aarch64-darwin-config-base = (nix-darwin.lib.darwinSystem {
+            system = "aarch64-darwin";
+            modules = [ self.darwinModules.default ];
+          }).system;
+        } else { };
+    in
+      {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            devshell.overlays.default
+            emacs-overlay.overlays.default
+          ];
+        };
 
-          apps = {
-            hello = {
-              type = "app";
-              program = "${pkgs.hello}/bin/hello";
-            };
-          };
-
-          packages = {
-            website = pkgs.callPackage ../website {
-              emacs = self'.packages.emacs-ross;
-              src = ../../src;
-            };
-          } // darwinPackages;
-
-          devShells.default = pkgs.devshell.mkShell {
-            name = "cromulent";
-
-            commands = [
-              {
-                name = "hm-switch";
-                help = "switch the home-manager config";
-                command = "${hm}/bin/home-manager switch --flake $PRJ_ROOT";
-              }
-              {
-                name = "serve";
-                help = "run 'hugo serve' on the local project";
-                command = "(cd $PRJ_ROOT && ${pkgs.hugo}/bin/hugo serve --disableFastRender --config tmp/hugo/config.toml)";
-              }
-            ];
-
-            packages = [
-              hm
-              pkgs.google-cloud-sdk
-              pkgs.hugo
-              pkgs.nix
-              pkgs.terraform
-            ];
-          };
-
-          overlayAttrs = {
+        apps = {
+          hello = {
+            type = "app";
+            program = "${pkgs.hello}/bin/hello";
           };
         };
-  }
+
+        packages = {
+          website = pkgs.callPackage ../website {
+            emacs = self'.packages.emacs-ross;
+            src = ../../src;
+          };
+        } // darwinPackages;
+
+        devShells.default = pkgs.devshell.mkShell {
+          name = "cromulent";
+
+          commands = [
+            {
+              name = "hm-switch";
+              help = "switch the home-manager config";
+              command = "${hm}/bin/home-manager switch --flake $PRJ_ROOT";
+            }
+            {
+              name = "serve";
+              help = "run 'hugo serve' on the local project";
+              command = "(cd $PRJ_ROOT && ${pkgs.hugo}/bin/hugo serve --disableFastRender --config tmp/hugo/config.toml)";
+            }
+          ];
+
+          packages = [
+            hm
+            pkgs.google-cloud-sdk
+            pkgs.hugo
+            pkgs.nix
+            pkgs.terraform
+          ];
+        };
+
+        overlayAttrs = {
+        };
+      };
+}
 )
