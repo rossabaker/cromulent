71d67e734fe246d6c2ef1a0e0051c3f4b4dc4ea1
Ross A. Baker <ross@rossabaker.com>
2023-09-25 18:48:41 -0400

    Remove Slack and Zoom from "work" module

--- modules/work/default.nix
+++ modules/work/default.nix
@@ -8,12 +8,5 @@
   home.packages = [
     pkgs.firefox-bin
     pkgs.rectangle
-    pkgs.slack
-    pkgs.zoom-us
-  ];
-
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "slack"
-    "zoom"
   ];
 }
