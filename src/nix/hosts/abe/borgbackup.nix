8db4f4557a81e1668535ba8f500c28e8cc51a8c6
Ross A. Baker <ross@rossabaker.com>
2023-11-19 00:31:45 -0500

    Encrypt borgbackup

--- src/nix/hosts/abe/borgbackup.nix
+++ src/nix/hosts/abe/borgbackup.nix
@@ -3,7 +3,10 @@
 {
   services.borgbackup.jobs.test = {
     paths = [ "/root/cromulent" ];
-    encryption.mode = "none";
+    encryption = {
+      mode = "repokey-blake2";
+      passCommand = "cat /root/borgbackup/passphrase";
+    };
     repo = "ssh://u377329@u377329.your-storagebox.de:23/home/backups/abe";
     doInit = true;
     compression = "auto,zstd";
