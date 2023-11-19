30998119447edef92c15a4be62f3f8e488f39a9f
Ross A. Baker <ross@rossabaker.com>
2023-11-18 23:56:15 -0500

    ssh to port 23 for borg

--- src/nix/hosts/abe/borgbackup.nix
+++ src/nix/hosts/abe/borgbackup.nix
@@ -4,7 +4,7 @@
   services.borgbackup.jobs.test = {
     paths = [ "/root/cromulent" ];
     encryption.mode = "none";
-    repo = "ssh://u377329@u377329.your-storagebox.de/home/backups/abe";
+    repo = "ssh://u377329@u377329.your-storagebox.de:23/home/backups/abe";
     doInit = true;
     compression = "auto,zstd";
     startAt = "daily";
