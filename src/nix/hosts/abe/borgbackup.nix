92a36bb34829f3a6aa53c2582b3c878db75b6c71
Ross A. Baker <ross@rossabaker.com>
2023-11-18 23:54:17 -0500

    Initialize borg

--- src/nix/hosts/abe/borgbackup.nix
+++ src/nix/hosts/abe/borgbackup.nix
@@ -4,7 +4,8 @@
   services.borgbackup.jobs.test = {
     paths = [ "/root/cromulent" ];
     encryption.mode = "none";
-    repo = "ssh://u377329@u377329.your-storagebox.de:/home/backups/abe";
+    repo = "ssh://u377329@u377329.your-storagebox.de/home/backups/abe";
+    doInit = true;
     compression = "auto,zstd";
     startAt = "daily";
   };
