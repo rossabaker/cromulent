d6b19f77c1450d03aa023db20d7ab8818994ec1f
Ross A. Baker <ross@rossabaker.com>
2023-11-19 15:06:17 -0500

    Some initial backup paths

--- src/nix/hosts/abe/borgbackup.nix
+++ src/nix/hosts/abe/borgbackup.nix
@@ -4,7 +4,16 @@
   age.secrets.abe-backups-default.file = ../../../secrets/abe-backups-default.age;
 
   services.borgbackup.jobs.default = {
-    paths = [ "/root/cromulent" ];
+    paths = [
+      "/etc"
+      "/home"
+      "/var"
+      "/root"
+    ];
+    exclude = [
+      "/var/cache"
+      "/var/tmp"
+    ];
     encryption = {
       mode = "repokey-blake2";
       passCommand = "cat ${config.age.secrets.abe-backups-default.path}";
