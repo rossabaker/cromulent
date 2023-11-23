408f04bc6906585cf028b74c69381101d6bd5bd0
Ross A. Baker <ross@rossabaker.com>
2023-11-22 20:36:46 -0600

    Use netdata cloud

--- gen/flake/nixos/abe/netdata.nix
+++ gen/flake/nixos/abe/netdata.nix
@@ -1,9 +1,13 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
 {
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "netdata"
+  ];
+
   services.netdata = {
     enable = true;
-
+    package = pkgs.netdataCloud;
     config = {
       global = {
         "update every" = 15;
