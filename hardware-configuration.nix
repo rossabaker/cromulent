e3c056b041e5ef7016174267ad5795ad5bd0ddbe
Ross A. Baker <ross@rossabaker.com>
2023-11-27 18:15:43 -0500

    Drop unreferenced *configuration.nix files

--- hardware-configuration.nix
+++
@@ -1,7 +0,0 @@
-# This is just an example, you should generate yours with nixos-generate-config and put it in here.
-{
-  fileSystems."/" = {
-    device = "/dev/sda1";
-    fsType = "ext4";
-  };
-}
