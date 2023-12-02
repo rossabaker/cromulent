cf5afa6333b2d07b7771d12c4503244b14f7abd1
Ross A. Baker <ross@rossabaker.com>
2023-12-01 22:31:42 -0500

    Add nixos cache back to abe

--- gen/flake/hosts/abe/default.nix
+++ gen/flake/hosts/abe/default.nix
@@ -12,6 +12,13 @@
             self.nixosModules.synapse
             self.nixosModules.forgejo
             inputs.agenix.nixosModules.default
+            {
+              # TODO This is missing...
+              nix.settings = {
+                substituters = [ "https://cache.nixos.org/" ];
+                trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+              };
+            }
           ];
         };
 
