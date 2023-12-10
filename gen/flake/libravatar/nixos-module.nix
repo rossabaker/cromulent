37fbe88abc1b1a40c5d67fe7f35b90e326992ca6
Ross A. Baker <ross@rossabaker.com>
2023-12-09 20:18:13 -0500

    Make libravatar a configurable module

--- gen/flake/libravatar/nixos-module.nix
+++ gen/flake/libravatar/nixos-module.nix
@@ -1,20 +1,36 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 let
-  domain = "avatars.rossabaker.com";
-  avatar = pkgs.callPackage ./avatar.nix {
-    src = ../../../src/avatars;
-  };
+  cfg = config.com.rossabaker.services.libravatar;
 in
 {
-  services.nginx.virtualHosts.${domain} = {
-    forceSSL = true;
-    enableACME = true;
-    locations."~ ^/avatar/([0-9a-f]+)" = {
-      extraConfig = ''
-        root ${avatar};
-        try_files /$1.jpg /$1.png /$1.gif =404;
-      '';
+  options.com.rossabaker.services.libravatar = {
+    enable = lib.mkEnableOption "Libravatar Nginx host" // {
+      default = true;
+    };
+    domain = lib.mkOption {
+      type = lib.types.str;
+      example = "avatars.example.com";
+    };
+    avatars = lib.mkOption {
+      type = lib.types.path;
+    };
+  };
+
+  config = let
+    avatars = pkgs.callPackage ./avatar.nix {
+      src = cfg.avatars;
+    };
+  in lib.mkIf cfg.enable {
+    services.nginx.virtualHosts."${cfg.domain}" = {
+      forceSSL = true;
+      enableACME = true;
+      locations."~ ^/avatar/([0-9a-f]+)" = {
+        extraConfig = ''
+          root ${avatars};
+          try_files /$1.jpg /$1.png /$1.gif =404;
+       '';
+      };
     };
   };
 }
