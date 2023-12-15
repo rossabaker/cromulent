266960e99b6c45561bfaa3e81955088b27ecc0b1
Ross A. Baker <ross@rossabaker.com>
2023-12-14 19:42:06 -0500

    Clean up dangling social CNAME

--- gen/flake/hosts/abe/dns/cnames.nix
+++ gen/flake/hosts/abe/dns/cnames.nix
@@ -6,7 +6,6 @@ in
 {
   networking.domains.subDomains =
     lib.listToAttrs (map (host: { name = host; value = { cname.data = cname; };}) [
-      "social.rossabaker.com"
       "beta.www.rossabaker.com"
     ]);
 }
