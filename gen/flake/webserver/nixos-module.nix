0a9603203a31ae09ac29e71472dad80615f84b85
Ross A. Baker <ross@rossabaker.com>
2024-01-06 20:36:55 -0500

    Add www user

--- gen/flake/webserver/nixos-module.nix
+++ gen/flake/webserver/nixos-module.nix
@@ -9,7 +9,22 @@ in
     virtualHosts.${domain} = {
       forceSSL = true;
       enableACME = true;
-      root = "/var/www/com.rossabaker.www.beta";
+      root = "${config.users.users.www.home}/public";
     };
   };
+
+  users.users.www = {
+    description = "Owns the web root for www";
+    isSystemUser = true;
+    home = "/var/lib/www";
+    createHome = true;
+    homeMode = "755";
+    group = "www";
+    useDefaultShell = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID+UMMyU0dpJMX2hVRAu2tTqvcTIueNLdJmG049iSrtu"
+    ];
+  };
+
+  users.groups.www = { };
 }
