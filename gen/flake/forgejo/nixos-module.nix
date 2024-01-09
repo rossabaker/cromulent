2503e58dc957e969a5ad8a2edfc62411aadceaf7
Ross A. Baker <ross@rossabaker.com>
2024-01-08 23:17:28 -0500

    Turn off noisy access log from forgejo runner polling

--- gen/flake/forgejo/nixos-module.nix
+++ gen/flake/forgejo/nixos-module.nix
@@ -17,11 +17,23 @@ in
     };
   };
 
+  services.nginx = {
+    appendHttpConfig = ''
+      map $uri $forgejo_access_log {
+        default 1;
+        /api/actions/runner.v1.RunnerService/FetchTask 0;
+      }
+    '';
+  };
+
   services.nginx.virtualHosts.${domain} = {
     enableACME = true;
     forceSSL = true;
     locations."/" = {
       proxyPass = "http://localhost:3000/";
+      extraConfig = ''
+        access_log /var/log/nginx/access.log combined if=$forgejo_access_log;
+      '';
     };
   };
 }
